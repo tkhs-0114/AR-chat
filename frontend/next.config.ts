a81676c1611a5ab3a650e86b1672c51c13bdcc8b
tkhs-0114 <135799380+tkhs-0114@users.noreply.github.com>
2025-03-01 18:59:40 +0900

    add Docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 300,
+      aggregateTimeout: 300
+    }
+    return config
+  }
 };
 
 export default nextConfig;
